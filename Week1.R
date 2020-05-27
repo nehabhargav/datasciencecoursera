815b81b933175a8c241f5c91114c9c18522a0b76
neha <neha@Rs-MacBook-Pro.local>
2020-05-28 03:56:12 +0530

    final

--- Week1.R
+++ Week1.R
@@ -82,10 +82,83 @@ m[,5]# prints the whole 5th column
  cbind(x , y)
  rbind(x, y)
  
+ 
+ 
  #Factors = type of vector that represents categorical data
  # 2 types ordered, unoedered....takes input as a character
   x <- factor(c("yes","yes","no","yes"))
  table(x) # this gives me the count of the frequency of yes and no
 unclass(x)# brings it down to integer
+
+#Missing Values
+#is.na() used for null objects
+#is.nan() used for undefined mathematical operations
+
+#data frames --- used to store tabular data 
+#they are special types of list where every element of the list has the same length
+
+#attributes of data types like ----- read.csv, read.table(), rows.names()
+
+x <- data.frame( foo = 1:4, bar = c(T,T,F,F) )
+x
+
+ nrow(x) #uses two rows
+ ncol(x)
+
+ #Names
+ 
+ x<- 1:3
+ names(x)
+ names(x) <- c("foo","bar","nox")
+ x
+ names(x)
+ 
+ x <- list( a= 1, b=2, c=3 )
+x
+
+m <- matrix(1:4 , nrow = 2, ncol = 2)
+dimnames(m) <- list(c("a","b"), c("neha ", "priyanka"))
+m
+
+#sunsetting a vector 
+ x <- c("a","b","s","s","a")
+x
+x[2]
+x[1:4]
+x[ x > "b"]
+y <- x > "a"
+y
+
+
+#subsetting a list
+
+
+x <- list( foo =1:4 , bar = 0.6)
+x[1] # list od sequence
+
+x[[1]] #only sequence
+
+x$bar
+x["bar"]
+x[["bar"]]
+
+x <- list(foo=1:4, bar= 0.6, baz="hello") #to extract multiple elements of the list
+x[c(1,3)]
+ name <- "foo"
+x[[name]] 
+x$foo
+
+
+# removing na values
+x <- c(1,2,NA,4,5)
+bad <- is.na(x)
+x[ !bad ]
+
+x <- c(1,2,NA,4,5,NA)
+y <- c("1","2","3","4","5",NA)
+good <- complete.cases(x,y)
+good
+x[good]
+y[good]
 
 
