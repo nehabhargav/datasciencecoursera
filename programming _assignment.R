0909274ce771ae9b8862fcf9ed5cf202c3da1cf8
neha bhargava <53258421+nehabhargav@users.noreply.github.com>
2020-05-31 03:55:27 +0530

    This is my assignment

--- programming _assignment.R
+++ programming _assignment.R
@@ -1,7 +1,34 @@
-install.packages("Ryacas")
-library(Ryacas)
-x1 <- makeInverse(matrix(c(1,2,3,4),2,2))
-x1$getsolve() 
-cachesolve(x1)
-cachesolve(x1)
-x1$set(x1$getsolve()) 
+## functions do
+
+## Write a short comment describing this function
+
+makeCacheMatrix <- function(x = matrix()) {
+  inv<-NULL
+  set<-function(y){
+    x<<-y
+    inv<<-NULL
+    
+  }
+  get<-function() x
+  setinverse<-function(inverse) inv <<-inverse
+  getinverse<-function()inv
+  list(set=set,get=get,setinverse=setinverse,getinverse=getinverse)
+}
+
+
+## Write a short comment describing this function
+
+cacheSolve <- function(x, ...) {
+  ## Return a matrix that is the inverse of 'x'
+  inv<-x$getinverse()
+  if(!is.null(inv)){
+    message("This is getting cached data")
+    return (inv)
+  }
+  data<-x$get()
+  inv<-solve(data,....)
+  x$setinverse(inv)
+  inv
+}
+ 
+
